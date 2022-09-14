0635c66f2b5961f7f4c66e1a1bbff767905cda5f
UlyanaSa <94441431+UlyanaSa@users.noreply.github.com>
2022-09-14 17:12:35 +0300

    Add permissions internet and location

--- app/src/main/java/com/osvin/mapapp/MapActivity.kt
+++ app/src/main/java/com/osvin/mapapp/MapActivity.kt
@@ -30,11 +30,74 @@ class MapActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMapBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        MapKitFactory.setApiKey(MY_API_KEY)
+        MapKitFactory.initialize(this)
         super.onCreate(savedInstanceState)
         binding = ActivityMapBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
     }
 
+    override fun onStart() {
+        super.onStart()
+        MapKitFactory.getInstance().onStart()
+        binding.mapview.onStart()
+    }
 
+    override fun onStop() {
+        super.onStop()
+        binding.mapview.onStop()
+        MapKitFactory.getInstance().onStop()
+    }
+
+    private fun isLocationEnabled(): Boolean {
+        val locationManager: LocationManager =
+            getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
+            LocationManager.NETWORK_PROVIDER
+        )
+    }
+    private fun checkPermissions(): Boolean {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED &&
+            ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            return true
+        }
+        return false
+    }
+
+    private fun requestPermissions() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ),
+            MainActivity.permissionId
+        )
+    }
+
+//    @SuppressLint("MissingSuperCall")
+//    override fun onRequestPermissionsResult(
+//        requestCode: Int,
+//        permissions: Array<String>,
+//        grantResults: IntArray
+//    ) {
+//        if (requestCode == MainActivity.permissionId) {
+//            if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+//            }
+//        }
+//    }
+
+
+    companion object {
+        const val MY_API_KEY = "99257b26-2933-48f9-a649-364582209dc6"
+        const val permissionId = 10
+    }
 }
